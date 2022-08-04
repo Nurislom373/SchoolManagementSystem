87a527e9a84018a13e23fd7fe9ba84613d9268ba
Nurislom373 <nurislomx72@gmail.com>
2022-08-04 12:27:38 +0500

    some changes

--- Gateway-Service/src/main/java/org/khasanof/gatewayservice/config/SpringCloudConfig.java
+++ Gateway-Service/src/main/java/org/khasanof/gatewayservice/config/SpringCloudConfig.java
@@ -1,20 +1,26 @@
 package org.khasanof.gatewayservice.config;
 
+import lombok.RequiredArgsConstructor;
+import org.khasanof.gatewayservice.config.filter.AuthenticationFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class SpringCloudConfig {
+
+    private final AuthenticationFilter filter;
 
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder locatorBuilder) {
         return locatorBuilder.routes()
-                .route("Classroom-Service", r -> r.path("/classroom/**", "/classroomStudent/**")
-                        .uri("http://localhost:8005/"))
-                .route("Auth-Service", r -> r.path("/auth/**")
-                        .uri("http://localhost:8800/"))
+                .route("Classroom-Service", r -> r.path("/api/v1/classroom/**", "/api/v1/classroomStudent/**", "/api/v1/course/**", "/api/v1/grade/**")
+                        .uri("lb://Classroom-Service"))
+                .route("Auth-Service", r -> r.path("/api/v1/auth/**")
+                        .uri("lb://Auth-Service"))
                 .build();
     }
+//    http://localhost:8800/
 }
---
+++ Gateway-Service/src/main/java/org/khasanof/gatewayservice/config/filter/PreFilter.java
@@ -0,0 +1,66 @@
+package org.khasanof.gatewayservice.config.filter;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.khasanof.gatewayservice.config.jwt.JwtUtil;
+import org.khasanof.gatewayservice.config.routerValidator.RouterValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class PreFilter implements GlobalFilter {
+    final Logger logger = LoggerFactory.getLogger(PreFilter.class);
+    final RouterValidator routerValidator;
+    final JwtUtil jwtUtil;
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+
+        if (routerValidator.isSecured.test(request)) {
+            if (this.isAuthMissing(request))
+                return this.onError(exchange, "Authorization header is missing in request", HttpStatus.UNAUTHORIZED);
+
+            String token = this.getAuthHeader(request);
+            token = token.substring(7);
+
+            if (jwtUtil.isInvalid(token))
+                return this.onError(exchange, "Authorization header is invalid", HttpStatus.UNAUTHORIZED);
+
+            this.populateRequestWithHeaders(exchange, token);
+        }
+
+        logger.info("Global Filter executed");
+        return chain.filter(exchange);
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return response.setComplete();
+    }
+
+    private String getAuthHeader(ServerHttpRequest request) {
+        return request.getHeaders().getOrEmpty("Authorization").get(0);
+    }
+
+    private boolean isAuthMissing(ServerHttpRequest request) {
+        return !request.getHeaders().containsKey("Authorization");
+    }
+
+    private void populateRequestWithHeaders(ServerWebExchange exchange, String token) {
+        Claims claims = jwtUtil.getAllClaimsFromToken(token);
+        exchange.getRequest().mutate()
+                .header("id", String.valueOf(claims.getSubject()))
+                .header("role", String.valueOf(claims.get("role")))
+                .build();
+    }
+}
